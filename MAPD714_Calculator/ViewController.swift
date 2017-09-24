3535fea13f805874aeed4821ebaa36466fe4d2a4
Student <student@SETASs-Mac-mini.local>
2017-09-24 14:50:38 -0400

    Basic UI

---
+++ MAPD714_Calculator/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MAPD714_Calculator
+//
+//  Created by Student on 2017-09-23.
+//  Copyright © 2017 Centennial. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    //INHERITED METHODS
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    
+}
+
